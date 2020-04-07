cfaf755d78e5105ad01e6742cbf6e33cc816158f
Сергей Вилков <you@example.com>
2020-04-07 09:27:15 +0300

    Eslint fixes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 "use_strict";
 
-import { createSiteMenuTemplate } from "./components/site-menu.js";
-import { createControlsMenuTemplate } from "./components/controls-menu.js";
-import { createTripEventTemplate } from "./components/trip-event.js";
-import { createTripDaysTemplate } from "./components/trip-days.js";
-import { createPointsListItemsTemplate } from "./components/points-list-item.js";
-import { createEditEventTemplate } from "./components/edit-event.js";
+import {createSiteMenuTemplate} from "./components/site-menu.js";
+import {createControlsMenuTemplate} from "./components/controls-menu.js";
+import {createTripEventTemplate} from "./components/trip-event.js";
+import {createTripDaysTemplate} from "./components/trip-days.js";
+import {createPointsListItemsTemplate} from "./components/points-list-item.js";
+import {createEditEventTemplate} from "./components/edit-event.js";
 
 
 const POINTS_COUNT = 3;
 
 const render = (container, template, place) => {
-    container.insertAdjacentHTML(place, template);
+  container.insertAdjacentHTML(place, template);
 };
 
 const siteMainElement = document.querySelector(`.page-body`);
@@ -28,8 +28,6 @@ render(siteTripsElement, createTripEventTemplate(), `afterbegin`);
 render(siteTripsElement, createTripDaysTemplate(), `beforeend`);
 
 const siteListElement = document.querySelector(`.trip-events__list`);
-
-const siteListItemElement = siteListElement.querySelector(`.trip-events__item`);
 
 for (let i = 0; i < POINTS_COUNT; i++) {
   render(siteListElement, createPointsListItemsTemplate(), `beforeend`);
