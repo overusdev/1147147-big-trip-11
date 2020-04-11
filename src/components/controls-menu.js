c3685e65951cef835cda2a5f87ee271a8915f6a6
Сергей Вилков <you@example.com>
2020-04-11 20:27:22 +0300

    Filters controls map method

--- src/components/controls-menu.js
+++ src/components/controls-menu.js
@@ -1,4 +1,31 @@
+const createFilterMarkup = (id, checked, title) => {
+  return (
+    ` <div class="trip-filters__filter">
+        <input id="filter-${id}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${id}" ${checked ? `checked` : `` }>
+        <label class="trip-filters__filter-label" for="filter-everything">${title}</label>
+      </div>
+    `
+  );
+};
 export const createControlsMenuTemplate = () => {
+  const tripFilters = [
+    {
+      id: `everything`,
+      isChecked: true,
+      title: `Everything`
+    },
+    {
+      id: `future`,
+      isChecked: false,
+      title: `Future`
+    },
+    {
+      id: `past`,
+      isChecked: false,
+      title: `Past`
+    },
+  ].map((it) => createFilterMarkup(it.id, it.isChecked, it.title)).join(`\n`);
+
   return (
     `<h2 class="visually-hidden">Switch trip view</h2>
         <nav class="trip-controls__trip-tabs  trip-tabs">
@@ -8,21 +35,7 @@ export const createControlsMenuTemplate = () => {
 
         <h2 class="visually-hidden">Filter events</h2>
         <form class="trip-filters" action="#" method="get">
-          <div class="trip-filters__filter">
-            <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-            <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-          </div>
-
-          <div class="trip-filters__filter">
-            <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-            <label class="trip-filters__filter-label" for="filter-future">Future</label>
-          </div>
-
-          <div class="trip-filters__filter">
-            <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-            <label class="trip-filters__filter-label" for="filter-past">Past</label>
-          </div>
-
+          ${tripFilters}
           <button class="visually-hidden" type="submit">Accept filter</button>
         </form>`
   );
