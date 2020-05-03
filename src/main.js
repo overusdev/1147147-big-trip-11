7c29b4cb0d382406c585294662c3faa9fe00655c
Сергей Вилков <you@example.com>
2020-05-03 12:04:36 +0300

    Site menu render use class

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 'use_strict';
 
-import {createSiteMenuTemplate} from './components/site-menu.js';
+import {SiteMenuMarkupComponent} from './components/site-menu.js';
 import {createControlsMenuTemplate} from './components/controls-menu.js';
 import {createNewPointTemplate} from './components/new-way-point.js';
 import {createTripDaysTemplate} from './components/trip-days.js';
@@ -8,18 +8,25 @@ import {createEditEventTemplate} from './components/edit-event.js';
 import {generateFilters} from './mock/filter.js';
 import {generateNewPoint, getTrip} from './mock/trip-point.js';
 
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+const renderComponent = (container, childComponent) => {
+  container.appendChild(childComponent.createElement());
+};
+
 const siteMainElement = document.querySelector(`.page-body`);
 const siteHeaderElement = document.querySelector(`.page-header`);
-const siteTripInfoElement = siteHeaderElement.querySelector(`.trip-main`);
+const siteTripInfoElement = siteHeaderElement.querySelector(`.trip-main__trip-info`);
 const siteControlsElement = siteHeaderElement.querySelector(`.trip-main__trip-controls`);
 const siteTripsElement = siteMainElement.querySelector(`.trip-events`);
 const filters = generateFilters();
 
-render(siteTripInfoElement, createSiteMenuTemplate(), `afterbegin`);
+const createSiteMenuComponent = new SiteMenuMarkupComponent();
+renderComponent(siteTripInfoElement, createSiteMenuComponent);
+
 render(siteControlsElement, createControlsMenuTemplate(filters), `afterbegin`);
 render(siteTripsElement, createTripDaysTemplate(getTrip()), `beforeend`);
 
