94076a181276b6fbf2c4a4a9a179ed1f32dff469
Сергей Вилков <you@example.com>
2020-05-03 15:14:27 +0300

    Delete render function

--- src/components/new-way-point.js
+++ src/components/new-way-point.js
@@ -96,33 +96,33 @@ const eventTypeGroupDataSet = () => {
     </fieldset>
   `);
 };
-export class SortListMarkupComponent extends BaseComponent {
-  getTemplate() {
-    return sortListForm();
-  }
-}
+// export class SortListMarkupComponent extends BaseComponent {
+//   getTemplate() {
+//     return sortListForm();
+//   }
+// }
 
-const sortListForm = () => {
-  const elements = sortTypeData.map((item) => (`
-    <div class="trip-sort__item  trip-sort__item--time">
-        <input id="${item.id}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="${item.value}">
-        <label class="trip-sort__btn" for="${item.id}">
-            ${item.label}
-            <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-                <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-            </svg>
-        </label>
-    </div>
-  `));
+// const sortListForm = () => {
+//   const elements = sortTypeData.map((item) => (`
+//     <div class="trip-sort__item  trip-sort__item--time">
+//         <input id="${item.id}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="${item.value}">
+//         <label class="trip-sort__btn" for="${item.id}">
+//             ${item.label}
+//             <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+//                 <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+//             </svg>
+//         </label>
+//     </div>
+//   `));
 
-  return (`
-    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-        <span class="trip-sort__item  trip-sort__item--day">Day</span>
-        ${elements.join(`\n`)}
-        <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
-    </form>
-  `);
-};
+//   return (`
+//     <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+//         <span class="trip-sort__item  trip-sort__item--day">Day</span>
+//         ${elements.join(`\n`)}
+//         <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+//     </form>
+//   `);
+// };
 
 export class OfferMarkupComponent extends BaseComponent {
   constructor(item) {
@@ -209,76 +209,72 @@ export class NewPointMarkupComponent extends BaseComponent {
     return createNewPointTemplate(this._item);
   }
 }
-
+//      ${sortListForm()}
 export const createNewPointTemplate = (item) => {
-
   return (
-    `<h2 class="visually-hidden">Trip events</h2>
-        ${sortListForm()}
+    `<form class="trip-events__item  event  event--edit" action="#" method="post">
+        <header class="event__header">
+            <div class="event__type-wrapper">
+                <label class="event__type  event__type-btn" for="event-type-toggle-1">
+                    <span class="visually-hidden">Choose event type</span>
+                    <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+                </label>
 
-        <form class="trip-events__item  event  event--edit" action="#" method="post">
-            <header class="event__header">
-                <div class="event__type-wrapper">
-                    <label class="event__type  event__type-btn" for="event-type-toggle-1">
-                        <span class="visually-hidden">Choose event type</span>
-                        <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
-                    </label>
+                <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
-                    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-
-                    <div class="event__type-list">
-                        ${eventTypeDataSet()}
-                        ${eventTypeGroupDataSet()}
-                    </div>
+                <div class="event__type-list">
+                    ${eventTypeDataSet()}
+                    ${eventTypeGroupDataSet()}
                 </div>
+            </div>
 
-                <div class="event__field-group  event__field-group--destination">
-                    ${createPlacesMarkup()}
-                </div>
+            <div class="event__field-group  event__field-group--destination">
+                ${createPlacesMarkup()}
+            </div>
 
-                <div class="event__field-group  event__field-group--time">
-                    <label class="visually-hidden" for="event-start-time-1">
-                        From
-                    </label>
-                    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 00:00">
-                        &mdash;
-                    <label class="visually-hidden" for="event-end-time-1">
-                        To
-                    </label>
-                    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 00:00">
-                </div>
+            <div class="event__field-group  event__field-group--time">
+                <label class="visually-hidden" for="event-start-time-1">
+                    From
+                </label>
+                <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 00:00">
+                    &mdash;
+                <label class="visually-hidden" for="event-end-time-1">
+                    To
+                </label>
+                <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 00:00">
+            </div>
 
-                <div class="event__field-group  event__field-group--price">
-                    <label class="event__label" for="event-price-1">
-                        <span class="visually-hidden">Price</span>
-                        &euro;
-                    </label>
-                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
-                </div>
+            <div class="event__field-group  event__field-group--price">
+                <label class="event__label" for="event-price-1">
+                    <span class="visually-hidden">Price</span>
+                    &euro;
+                </label>
+                <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+            </div>
 
-                <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-                <button class="event__reset-btn" type="reset">Cancel</button>
-            </header>
+            <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+            <button class="event__reset-btn" type="reset">Cancel</button>
+        </header>
 
-            <section class="event__details">
-                <section class="event__section  event__section--offers">
-                    <h3 class="event__section-title  event__section-title--offers">
-                        Offers
-                    </h3>
-                    ${offerContainer(item.offers)}
-                </section>
-
-                <section class="event__section  event__section--destination">
-                    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-                    <p class="event__destination-description">
-                        ${item.destination.description}
-                    </p>
-
-                    <div class="event__photos-container">
-                        ${picturesContainer(item.destination.pictures)}
-                    </div>
-                </section>
+        <section class="event__details">
+            <section class="event__section  event__section--offers">
+                <h3 class="event__section-title  event__section-title--offers">
+                    Offers
+                </h3>
+                ${offerContainer(item.offers)}
             </section>
-    </form>`
+
+            <section class="event__section  event__section--destination">
+                <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+                <p class="event__destination-description">
+                    ${item.destination.description}
+                </p>
+
+                <div class="event__photos-container">
+                    ${picturesContainer(item.destination.pictures)}
+                </div>
+            </section>
+        </section>
+      </form>`
   );
 };
--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@
 import {SiteMenuMarkupComponent} from './components/site-menu.js';
 import {ControlsMenuComponent} from './components/controls-menu.js';
 import {SiteMenuButtonMarkupComponent} from './components/new-event-button.js';
-import {createNewPointTemplate} from './components/new-way-point.js';
-import {createTripDaysTemplate} from './components/trip-days.js';
+import {NewPointMarkupComponent} from './components/new-way-point.js';
+import {SortListMarkupComponent} from './components/sort.js';
+import {TripDaysMarkupComponent} from './components/trip-days.js';
 import {EditEventMarkupComponent} from './components/edit-event.js';
 import {generateFilters} from './mock/filter.js';
 import {generateNewPoint, getTrip} from './mock/trip-point.js';
-
-
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const renderComponent = (container, childComponent) => {
   container.appendChild(childComponent.createElement());
@@ -23,6 +19,14 @@ const siteMenuElement = document.querySelector(`.trip-main`);
 const siteTripsElement = siteMainElement.querySelector(`.trip-events`);
 const filters = generateFilters();
 
+const createSortComponent = new SortListMarkupComponent();
+renderComponent(siteTripsElement, createSortComponent);
+
+const newWayPointData = generateNewPoint();
+const createNewPointComponent = new NewPointMarkupComponent(newWayPointData);
+// const createNewPointComponent = new EventTypeMarkupComponent();
+renderComponent(siteTripsElement, createNewPointComponent);
+
 const createSiteMenuComponent = new SiteMenuMarkupComponent();
 renderComponent(siteMenuElement, createSiteMenuComponent);
 
@@ -32,12 +36,11 @@ renderComponent(siteMenuElement, createControlsMenuComponent);
 const createButtonMenuComponent = new SiteMenuButtonMarkupComponent();
 renderComponent(siteMenuElement, createButtonMenuComponent);
 
-render(siteTripsElement, createTripDaysTemplate(getTrip()), `beforeend`);
+
+const createTripDaysComponent = new TripDaysMarkupComponent(getTrip());
+renderComponent(siteTripsElement, createTripDaysComponent);
 
 const siteListElement = document.querySelector(`.trip-events__list`);
-const newWayPointData = generateNewPoint();
-
-render(siteTripsElement, createNewPointTemplate(newWayPointData), `afterbegin`);
 
 const createEditEventComponent = new EditEventMarkupComponent(newWayPointData);
 renderComponent(siteListElement, createEditEventComponent);
