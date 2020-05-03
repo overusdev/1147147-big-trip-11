524ca7c2478c46d8d0392615d98247462b0d00bf
Сергей Вилков <you@example.com>
2020-05-03 15:21:24 +0300

    Minor

--- src/components/new-way-point.js
+++ src/components/new-way-point.js
@@ -1,6 +1,6 @@
 import {BaseComponent} from "./base-component";
 
-import {eventTypeData, sortTypeData, eventTypeGroupData} from "../data/components-data";
+import {eventTypeData, eventTypeGroupData} from "../data/components-data";
 import {getMockPlaces} from "../mock/trip-point";
 
 export class PlaceOptionMarkupComponent extends BaseComponent {
@@ -96,33 +96,6 @@ const eventTypeGroupDataSet = () => {
     </fieldset>
   `);
 };
-// export class SortListMarkupComponent extends BaseComponent {
-//   getTemplate() {
-//     return sortListForm();
-//   }
-// }
-
-// const sortListForm = () => {
-//   const elements = sortTypeData.map((item) => (`
-//     <div class="trip-sort__item  trip-sort__item--time">
-//         <input id="${item.id}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="${item.value}">
-//         <label class="trip-sort__btn" for="${item.id}">
-//             ${item.label}
-//             <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-//                 <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-//             </svg>
-//         </label>
-//     </div>
-//   `));
-
-//   return (`
-//     <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-//         <span class="trip-sort__item  trip-sort__item--day">Day</span>
-//         ${elements.join(`\n`)}
-//         <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
-//     </form>
-//   `);
-// };
 
 export class OfferMarkupComponent extends BaseComponent {
   constructor(item) {
@@ -209,7 +182,7 @@ export class NewPointMarkupComponent extends BaseComponent {
     return createNewPointTemplate(this._item);
   }
 }
-//      ${sortListForm()}
+
 export const createNewPointTemplate = (item) => {
   return (
     `<form class="trip-events__item  event  event--edit" action="#" method="post">
