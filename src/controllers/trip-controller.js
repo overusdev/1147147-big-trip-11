64775d6180f996ae8102082c150f87723d5b55d7
Сергей Вилков <you@example.com>
2020-05-06 23:54:21 +0300

    Empty controller

---
+++ src/controllers/trip-controller.js
@@ -0,0 +1,10 @@
+import { renderComponent } from "./utils/render";
+export class TripController {
+  constructor(container) {
+    this._container = container;
+  }
+
+  render(container) {
+
+  }
+}
