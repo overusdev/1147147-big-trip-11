e69e377cf716f6c5b57dbd821beea89abd8177a8
Сергей Вилков <you@example.com>
2020-05-17 13:57:08 +0300

    Point controller add render

--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -1,9 +1,21 @@
-import {renderComponent} from "../utils/render";
+import {renderComponent, replace} from "../utils/render";
 import {WayPointMarkupComponent} from "../components/way-point";
 import {TripDaysMarkupComponent, TripDayMarkupComponent} from '../components/trip-days';
 import {EditEventMarkupComponent} from "../components/edit-event";
 import {getTrip} from '../mock/trip-point';
 const trip = getTrip();
+
+export class PointController {
+  constructor(container) {
+    this._container = container;
+    this._wayPointComponent = null;
+  }
+
+  render(wayPoint) {
+    this._wayPointComponent = new WayPointMarkupComponent(wayPoint);
+    renderComponent(this._container, this._wayPointComponent);
+  }
+}
 
 export class TripController {
   constructor(container) {
@@ -28,26 +40,20 @@ export class TripController {
     });
   }
 
-  renderTripWayPoint(container, wayPoint) {
-    const wayPointMarkupComponent = new WayPointMarkupComponent(wayPoint);
-    renderComponent(container, wayPointMarkupComponent);
-    wayPointMarkupComponent.setEditButtonClickHandler(() => {
-      this.replaceToEditWayPoint(container, wayPointMarkupComponent);
-    });
-  }
-
   renderTripDay(container, tripDay) {
     const tripDayMarkupComponent = new TripDayMarkupComponent(tripDay);
     const wayPoints = tripDay.wayPoints;
     renderComponent(container, tripDayMarkupComponent);
     const tripDayMarkupComponentElement = tripDayMarkupComponent.getElement();
     const tripDayContainerList = tripDayMarkupComponentElement.querySelector(`.trip-events__list`);
+    const renderTripWayPoint = new PointController(tripDayContainerList);
     wayPoints.forEach((wayPoint) => {
-      this.renderTripWayPoint(tripDayContainerList, wayPoint);
+      renderTripWayPoint.render(wayPoint);
     });
   }
 
   render() {
+
     const tripDays = trip.days;
     const tripDaysMarkupComponent = new TripDaysMarkupComponent();
     renderComponent(this._container, tripDaysMarkupComponent);
